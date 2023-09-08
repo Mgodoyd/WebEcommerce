0dd1dfd316b6829447a462d50eb6ce061f2269e2
Mgodoyd <godoymario110@gmail.com>
2023-09-08 11:51:53 -0600

    Get Clients

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { GLOBAL } from './GLOBAL';
 import { Observable } from 'rxjs';
-import { ICliente } from '../Entities/ICliente';
 @Injectable({
   providedIn: 'root'
 })
